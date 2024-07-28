98849d687ecc39e1bd80029a6fa882565eaf6c43
Zhou Chang <achang.zhou@gmail.com>
2024-07-28 11:59:04 +0800

    clean code

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # AgentMakefile
-Building LLM agent, Makefile is all you need!
+Building LLM agents, Makefile is all you need!
 
 
+仅仅使用Makefile可以构建各种智能体应用，tools 目录下是c++编写的各种 LLM 服务客户端以及扩展工具代码。
+
+
+以下是一些智能体例子:
+
+* agents/Makefile.exam ,  出试题卷子智能体，瀑布工作模式，你可以修改提示词，改成任意的科目，一般用于常见的大学考试科目。
+* agents/Makefile.trans,  中文翻译的例子，利用了 思考反射 方式。
+* agents/Makefile.baike,  使用外部工具的例子，百科知识问答
 
--- chat/kimi_chat.cpp
+++ chat/kimi_chat.cpp
@@ -1,17 +1,25 @@
+#include <unistd.h>
+
 #define CPPHTTPLIB_OPENSSL_SUPPORT
 #include "httplib.h"
 #include "json.hpp"
 using json = nlohmann::json;
 
+
 int main(int argc, const char* argv[]) {
-    if ( argc != 4 ) {
-        std::cout << "./chat history.json user.txt assistant.txt" << std::endl;
+    if ( argc < 5 ) {
+        std::cout << "./chat history.json user.txt assistant.txt new_history.json [output.json]" << std::endl;
         return -1;
     }
+
+    // FIXME: Request reached max request: 3, please try again after 1 seconds
+    sleep(2);
 
     const std::string history_file = argv[1];
     const std::string user_file = argv[2];
     const std::string assistant_file = argv[3];
+    const std::string _history_file = argv[4];
+    const std::string json_file = argc >= 6 ? argv[5] : "";
 
     const char* key = std::getenv("API_KEY");
     if ( key == nullptr) {
@@ -22,7 +30,7 @@ int main(int argc, const char* argv[]) {
 
     httplib::SSLClient cli("api.moonshot.cn");
     cli.enable_server_certificate_verification(false);
-    cli.set_read_timeout(120, 0);
+    cli.set_read_timeout(300, 0);
 
     httplib::Headers info;
     info.emplace("Authorization", api_key);
@@ -30,6 +38,11 @@ int main(int argc, const char* argv[]) {
     json data;
     data["model"] = "moonshot-v1-8k";
     data["temperature"] = 0.3;
+    if ( json_file == "" ) {
+        json format;
+        format["type"] = "json_object";
+        data["response_format"] = format;
+    }
 
     json messages;
     {
@@ -55,17 +68,37 @@ int main(int argc, const char* argv[]) {
 
     if ( res ) {
         json rdata = json::parse( res.value().body );
+        if ( rdata["choices"].is_null() ) {
+            std::cout << res.value().body << std::endl;
+            return -1;
+        }
         {
             messages.push_back(  rdata["choices"][0]["message"] );
-            std::ofstream oof(history_file);
+            std::ofstream oof(_history_file);
             oof << std::setw(4) << messages << std::endl;
         }
         {
             std::ofstream oof(assistant_file);
             oof << rdata["choices"][0]["message"]["content"].get<std::string>();
         }
+        // parsing JSON
+        if ( json_file != "") {
+            std::string c = rdata["choices"][0]["message"]["content"].get<std::string>();
+            auto first = c.find("```JSON") == std::string::npos ?  c.find("```json") : c.find("```JSON");
+            auto last = first;
+            if (first != std::string::npos) {
+                last = c.find("```", first+3);
+            }
+            if ( first != std::string::npos && last != std::string::npos ) {
+                c = c.substr(first + 7, last - first - 7);
+            }
+            std::ofstream oof(json_file);
+            oof << c;
+        }
     } else {
         std::cout << res.error() << std::endl;
+        return -1;
     }
+    return 0;
 }
 
